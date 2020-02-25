1d8b34892d3ef2a5d6f17283656b9b35a4ed888d
Ibnurizqiaramadan <32134721+ibnurizqiaramadan@users.noreply.github.com>
2020-02-25 15:04:34 +0700

    tambah routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'C_wisuda';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['wisuda/insert'] = 'C_wisuda/insertUpdate';
+$route['wisuda/update'] = 'C_wisuda/insertUpdate';
+$route['wisuda/add'] = 'C_wisuda/add';
+$route['wisuda/edit'] = 'C_wisuda/edit';
+$route['wisuda/delete/(:any)'] = 'C_wisuda/delete/$1';
+
